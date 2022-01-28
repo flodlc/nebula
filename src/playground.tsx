e8619bbb11525be62fab2e39e1be18d7c2ce6df5
flodlc <f.delacomble@gmail.com>
2022-01-28 16:28:47 +0100

    playground

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { ReactNebula } from "src/ReactNebula";
+import { smallSolarSystem } from "src/templates/smallSolarSystem";
+import { bigSolarSystem } from "src/templates/bigSolarSystem";
 
 export const Playground = () => {
-  return <ReactNebula />;
+  return (
+    <>
+      <div style={{ height: "100vh" }}>
+        <ReactNebula config={{ astres: [] }} />
+      </div>
+      <div style={{ height: "100vh" }}>
+        <ReactNebula config={{ astres: bigSolarSystem }} />
+      </div>
+      <div style={{ height: "100vh" }}>
+        <ReactNebula config={{ astres: smallSolarSystem }} />
+      </div>
+    </>
+  );
 };
