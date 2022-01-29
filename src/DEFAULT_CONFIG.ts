a1ca5ac2a436ce2675a01783f219e24618239faa
flodlc <f.delacomble@gmail.com>
2022-01-29 12:25:33 +0100

    screenshot

--- src/DEFAULT_CONFIG.ts
+++ src/DEFAULT_CONFIG.ts
@@ -5,7 +5,7 @@ export const DEFAULT_CONFIG = {
   starsRotationSpeed: 10,
   nebulasIntensity: 12,
   nebulasColors: ["rgb(27,2,140)", "rgb(22,91,2)", "#880554"],
-  solarSystemScale: 1,
+  solarSystemScale: 0,
   solarSystemDistance: 64,
   solarSystemRotationSpeed: 3,
 };
