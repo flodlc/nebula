67644af1c73605157c614daeb16b212a5d0796e4
flodlc <f.delacomble@gmail.com>
2022-01-30 23:28:01 +0100

    moved index

--- vite-build.config.ts
+++ vite-build.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "index.ts"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       fileName: (format) => `index.${format}.js`,
       formats: ["cjs", "es"],
     },
